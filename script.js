83f01cdbd8e78dc185151b8436809d970f362c43
Ryan Corrigan <ryandcorrigan@gmail.com>
2023-05-04 19:00:46 -0600

    removed unneeded trimming of coordinates strings

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 const div = document.getElementById('data');
 const update = document.getElementById('update');
 
+// fetches current coordinates of ISS
 fetch('http://api.open-notify.org/iss-now.json')
     .then(response => response.json())
     .then(data => { 
         const coordinates = document.createElement('p');
-        let lat = JSON.stringify(data['iss_position']['latitude']);
-        let long = JSON.stringify(data['iss_position']['longitude']);
-        lat = lat.substring(1, lat.length-1);
-        long = long.substring(1, long.length-1);
-        coordinates.textContent = `Latitude: ${lat},
+        coordinates.classList.add('coordinates');
+        let lat = data['iss_position']['latitude'];
+        let long = data['iss_position']['longitude'];
+        coordinates.textContent = `Latitude: ${lat}
         Longitude: ${long}`;
         
-        const getLocation = (latitude, longitude) => {
+        // reverse geocode for region
+        const getRegion = (latitude, longitude) => {
             fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
               .then(response => response.json())
               .then(data => {
                 const region = document.createElement('div');
-                if (data.countryName.length > 0) {
-                    region.textContent = `The ISS is currently above: ${data.countryName}`;
+                const appendP = document.createElement('p');
+                const appendP2 = document.createElement('p');
+                region.innerText = 'The ISS is currently above';
+                appendP.classList.add('location');
+
+                if (data.countryName.length) {
+                    appendP.innerText = data.countryName;
+                    region.append(appendP);
+
                 } else {
-                    region.textContent = `The ISS is currently above: the ${data.localityInfo.informative[0].name} (the ${data.localityInfo.informative[0].description})`;
-                }
+                    appendP.innerText = `The ${data.localityInfo.informative[0].name}`;
+                    appendP2.innerText = `The ${data.localityInfo.informative[0].description}`;
+                    region.append(appendP);
+                    region.append(appendP2);
+                } 
                 div.appendChild(region).appendChild(coordinates);
               })
               .catch(error => {
                 console.log(`Error fetching location data: ${error}`);
               });
         }
-        getLocation(lat, long); 
+        getRegion(lat, long); 
     })
     .catch(error => console.error(error));
 
 update.addEventListener('click', window.location.reload.bind(window.location));
+
+
+
+
+
+
+
+
+             
